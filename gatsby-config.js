37855055d6cc1678c0056145aa5f196ec25383d2
Vishnu KS <yo@vishnuks.com>
2022-12-15 00:17:04 +0530

    Remove posts.

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,9 +9,9 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
+    title: `Vishnu`,
     author: {
-      name: `Kyle Mathews`,
+      name: `Vishnu`,
       summary: `who lives and works in San Francisco building useful things.`,
     },
     description: `A starter blog demonstrating what Gatsby can do.`,
